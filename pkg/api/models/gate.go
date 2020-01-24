79f372cd7ed10f60db5701c24a551b44494ba60a
Wyatt Barnes <wyatt@writerof.software>
2020-01-23 20:37:08 -0600

    Add gate model

---
+++ pkg/api/models/gate.go
@@ -0,0 +1,18 @@
+package models
+
+// Member description.
+// swagger:model member
+type Gate struct {
+	// ID of the gate
+	//
+	// required: true
+	ID string `json:"id,omitempty"`
+	// Name of the gate
+	//
+	// required: true
+	Name string `json:"name,omitempty"`
+	// Status of gate
+	//
+	// required: true
+	Status string `json:"status,omitempty"`
+}
